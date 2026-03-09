f9771964ab7efb129931011f5caa4d97fbd63487
Abu Huraira <162177269+Harry-47@users.noreply.github.com>
2026-03-09 17:20:57 +0500

    implemented doubly linked list

---
+++ Data Structures/Doubly-Linked List.cpp
@@ -0,0 +1,270 @@
+ #include<iostream>
+using namespace std;
+//Note: Dont Delete A node because it will cause a memory leak in this program
+// Node class definition
+class Node {
+public:
+    int key;
+    int data;
+    Node *next;
+    Node *prev;
+
+    // Default constructor
+    Node() : key(0), data(0), next(NULL), prev(NULL) {}
+
+    // Parameterized constructor
+    Node(int key, int data) : key(key), data(data), next(NULL), prev(NULL) {}
+};
+
+// Doubly linked list class definition
+class DoublyLinkedList {
+public:
+    Node *head;
+
+    // Default constructor
+    DoublyLinkedList() : head(NULL) {}
+
+    // Parameterized constructor
+    DoublyLinkedList(Node *node) : head(node) {}
+
+    // Function to check if a node with a given key exists in the list
+    Node *nodeExists(int k) {
+        Node *temp = head;
+        while (temp != NULL) {
+            if (temp->key == k)
+                return temp;
+            temp = temp->next;
+        }
+        return NULL;
+    }
+
+    // Function to append a node to the end of the list
+    void appendNode(Node *n) {
+        if (nodeExists(n->key) != NULL)
+            std::cout << "Node with the given key already exists" << std::endl;
+        else {
+            if (head == NULL) {
+                head = n;
+                std::cout << "Node appended!" << std::endl;
+            } 
+              else {
+                Node *temp = head;
+                while (temp->next != NULL)
+                    temp = temp->next;
+                temp->next = n;
+                n->prev = temp;
+                std::cout << "Node appended!" << std::endl;
+            }
+        }
+    }
+
+    // Function to prepend a node to the beginning of the list
+    void prependNode(Node *n) {
+        if (nodeExists(n->key) != NULL)
+            std::cout << "Node with the given key already exists" << std::endl;
+        else {
+            if (head == NULL)
+              {
+                head = n;
+                std::cout << "Node prepended!" << std::endl; 
+              }
+            else {
+                n->next = head;
+                head->prev = n;
+                head = n;
+                std::cout << "Node prepended!" << std::endl;
+            }
+        }
+    }
+
+    // Function to remove a node with a given key from the list
+    void removeNode(int k) {
+        if (nodeExists(k) == NULL)
+            std::cout << "Node with the given key does not exist" << std::endl;
+
+        else {
+            if (head->key == k) {
+
+                head = head->next;
+                head->prev = NULL;
+                std::cout << "Node removed!" << std::endl;
+            } 
+            else if (head == NULL)
+                std::cout << "Warning: List is Empty! " << std::endl;
+
+            else 
+            {
+                Node *temp = head;
+                while (temp!= NULL) {
+                    if (temp->key == k) 
+                    {
+                        temp->prev->next = temp->next;
+                        if(temp->next != NULL)
+                           temp->next->prev = temp->prev;
+
+                        temp->next = NULL;
+                        temp->prev = NULL;
+                        std::cout << "Node removed!" << std::endl;
+                        break;
+                    }
+                    temp = temp->next;
+                }
+            }
+        }
+    }
+
+    // Function to update the data of a node with a given key
+    void updateNode(int k, int data) {
+        Node *temp = nodeExists(k);
+        if (temp == NULL)
+            std::cout << "Node with the given key does not exist" << std::endl;
+        else {
+            temp->data = data;
+            std::cout << "Node updated!" << std::endl;
+        }
+    }
+
+    // Function to display all nodes in the list
+    void displayNodes() {
+       
+       if(head == NULL)
+       std::cout << "List is empty" << std::endl;
+       
+        Node *temp = head;
+        while (temp != NULL) {
+            std::cout << "(" << temp->key << ", " << temp->data << ")-->";
+            if(temp->next == NULL)
+            std::cout << " " << std::endl;
+
+            temp = temp->next;
+        }
+    }
+
+    // Function to count the number of nodes in the list
+    void countNodes() 
+    {
+        Node *temp = head;
+        int count = 0;
+        while (temp != NULL) 
+        {
+            count++;
+            temp = temp->next;
+        }
+        std::cout << "The number of nodes in the doubly linked list is: " << count << std::endl;
+    }
+
+    // Function to insert a node after a node with a given key
+    void insertNode(Node *node, int k) {
+        if (nodeExists(node->key) != NULL)
+            std::cout << "Node with the given key already exists" << std::endl;
+        else {
+            if (head == NULL) 
+            {
+                std::cout << "Warning: List is Empty! " << std::endl;
+                head = node;
+                std::cout << "Node Appended" << std::endl;
+            } 
+            else if (nodeExists(k) == NULL)
+            {
+                std::cout << "Node Does Not Exist" << std::endl;
+                return;
+            } 
+            else {
+                Node *curr = head;
+                while (curr != NULL) {
+                    if(curr->key == k)
+                    {
+                        node->next = curr->next;
+                        node->prev = curr;
+                        node->next->prev = node;
+                        curr->next = node;
+
+                        std::cout << "Node Inserted" << std::endl;
+                        break;
+                    }
+                    else if(curr->next == NULL){
+                       curr->next = node; 
+                       node->prev = curr;
+                    }
+                    }
+                 curr = curr->next;
+                }
+            }
+        }
+};
+int main() {
+    DoublyLinkedList List;
+    int choice, key, data;
+    Node *node;
+
+    while (true)
+     {
+        std::cout << " DoublyLinked List Operations:\n";
+        std::cout << "1. Append\n";
+        std::cout << "2. Prepend\n";
+        std::cout << "3. Delete\n";
+        std::cout << "4. Insert\n";
+        std::cout << "5. Update\n";
+        std::cout << "6. Count\n";
+        std::cout << "7. Display\n";
+        std::cout << "8. Exit\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                std::cout << "Enter key and data: ";
+                std::cin >> key >> data;
+                node = new Node(key, data);
+                List.appendNode(node);
+                break;
+            case 2:
+                std::cout << "Enter key and data: ";
+                std::cin >> key >> data;
+                node = new Node(key, data);
+                List.prependNode(node);
+                break;
+            case 3:
+                std::cout << "Enter key to delete: ";
+                std::cin >> key;
+                List.removeNode(key);
+                break;
+            case 4:
+                std::cout << "Enter key and data for new node: ";
+                std::cin >> key >> data;
+                node = new Node(key, data);
+                std::cout << "Enter key after which to insert: ";
+                std::cin >> key;
+                List.insertNode(node, key);
+                break;
+            case 5:
+                std::cout << "Enter key and new data: ";
+                std::cin >> key >> data;
+                List.updateNode(key, data);
+                break;
+            case 6:
+                List.countNodes();
+                break;
+            case 7:
+                List.displayNodes();
+                break;
+            case 8:
+                // Deallocate memory
+                while (List.head != NULL) {
+                    node = List.head;
+                    List.head = List.head->next;
+                    delete node;
+                }
+                return 0;
+            default:
+                std::cout << "Invalid choice!\n";
+        }
+    }
+        while (List.head != NULL) {
+                    node = List.head;
+                    List.head = List.head->next;
+                    delete node;
+                }
+
+    return 0;
+}
